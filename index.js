39166fbbe918052a76493d0aaeb2acb630ed512f
Julia <hijulia259@gmail.com>
2022-12-19 16:49:00 -0300

    falta css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let numbers = document.querySelectorAll(".calculadora-grid-item")
 let previousNumber = document.querySelector(".calculadora-board-previous-number")
 let operationSymbols = document.querySelectorAll(".calculadora-operation-symbol")
 let resultBtn = document.querySelector(".result-button")
+let deleteAllButton = document.querySelector(".deleteAll-button")
 let result;
 let current;
 let numbersArr = []
@@ -12,27 +13,31 @@ let currentOperationSymbol;
 let i = 0;
 let a = 0;
 let previous;
+let counter = 0;
 let b = a + 1;
 let r;
-numbers.forEach(button => button.addEventListener("click", function(e) {      
-if(currentNumber.innerText == previousNumber.innerText) {
-       result = ""
-      // alert("OI")
-       currentNumber.innerText = ""
-     ///  currentNumber.innerText = "" + button.innerText
-      
-      // // currentNumber.innerText = button.innerText
+numbers.forEach(button => button.addEventListener("click", function(e) {
         
-       // currentNumber.innerText = currentNumber.innerText + button.innerText
-    }
+if(button.innerText == ".") {
+        if(currentNumber.innerText == "") {
+                currentNumber.innerText = "0"
+        }
+}
+if(button.innerText == "%") {
 
-//else {
+}
+
+
+
+if(currentNumber.innerText > 1000000000000000) {
+        currentNumber.innerText = currentNumber.innerText
+}
         currentNumber.innerText = currentNumber.innerText + button.innerText
- // }
-}  
+}
+
 ));
 
-let counter = 0;
+//let counter = 0;
 let c = 0
 let d = 1
 
@@ -40,40 +45,51 @@ operationSymbols.forEach(button => button.addEventListener("click",function(e) {
         counter++
         currentOperationSymbol = button.innerText
         currentBoardSymbol.innerText = currentOperationSymbol
-      // if(currentNumber.innerText != "") {
-       // 
+        if(currentNumber.innerText !== "") {
         numbersArr.push(currentNumber.innerText)
-       //}
+        }
         previousNumber.innerText = currentNumber.innerHTML
         currentNumber.innerText = currentNumber.innerText
         symbolArr.push(currentOperationSymbol)
-       // currentNumber.innerText = ""
-        a ++
+     
 if(numbersArr.length > 1) {
 if(numbersArr.at(-1) != numbersArr.at(-2)) {
-//if(numbersArr.innerText !== "") {      
-        //if(currentNumber.innerText !== "") {
+        if(currentNumber.innerText !== "")
                r = operations[symbolArr.at(-2)](parseFloat(numbersArr.at(-2)),parseFloat(numbersArr.at(-1))) 
                previousNumber.innerText = r
-              // currentNumber.innerText = ""
                currentNumber.innerText = r
-               numbersArr.push(r)
-               a++
-                
-//}
+               numbersArr.push(r)    
+                      
 }
-//}
+ 
+if((currentBoardSymbol.innerText == "x²")||(currentBoardSymbol.innerText == "√")) {
+        r = operations[symbolArr.at(-1)](parseFloat(numbersArr.at(-1))) 
+        previousNumber.innerText = r
+        currentNumber.innerText = r 
+        currentBoardSymbol.innerText = ""
+        numbersArr.push(r)   
 }
-c++
+if((currentNumber.innerText == "%")) {
+        console.log(operations[symbolArr.at(-1)])
+        console.log(numbersArr.at(-1))
+        r = operations[symbolArr.at(-1)](parseFloat(numbersArr.at(-1)))
+        previousNumber.innerText = r
+        currentNumber.innerText = r
+        currentBoardSymbol.innerText = ""
+        numbersArr.push(r)  
+}
+}
+currentNumber.innerText = ""
 }
 
 ))
+deleteAllButton.addEventListener("click", () => {
+        currentNumber.innerText = "0"
+        previousNumber.innerText = "0"
+        currentBoardSymbol.innerText = ""
+})
 resultBtn.addEventListener("click",function(){
-        if((currentOperationSymbol === undefined) || (currentNumber.innerText === "")) {
-                currentNumber.innerText = currentNumber.innerText
-                previousNumber.innerText = previousNumber.innerText
-        }
-        else {
+        if((currentOperationSymbol !== undefined) || (currentNumber.innerText !== "")) {
         result = operations[currentOperationSymbol](parseFloat(previousNumber.innerText),parseFloat(currentNumber.innerText))
         currentNumber.innerText = result
         previousNumber.innerText = ""
@@ -87,5 +103,6 @@ const operations = {
         "%" : (x) => x / 100,
         "÷" : (x,y) => x / y,
         "x²" : (x) => x * x,
-        "√" : (x) => Math.sqrt(x)
+        "√" : (x) => Math.sqrt(x),
+
 }
