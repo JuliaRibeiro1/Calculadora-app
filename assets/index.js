f45fcf8f6412aa0e0eedc15ad7536b2851c2da7e
Julia <hijulia259@gmail.com>
2022-12-21 22:17:30 -0300

    falta ajustar deleteAllBtn

--- assets/index.js
+++ assets/index.js
@@ -4,18 +4,17 @@ let previousNumber = document.querySelector(".calculadora-board-previous-number"
 let operationSymbols = document.querySelectorAll(".calculadora-operation-symbol")
 let resultBtn = document.querySelector(".result-button")
 let deleteAllButton = document.querySelector(".deleteAll-button")
+let deleteLastNumberBtn = document.querySelector(".delete-last-number-button")
 let result;
 let current;
 let numbersArr = []
 let symbolArr = []
 let currentBoardSymbol = document.querySelector(".calculadora-board-symbol")
 let currentOperationSymbol;
-let i = 0;
-let a = 0;
 let previous;
 let counter = 0;
-let b = a + 1;
 let r;
+
 numbers.forEach(button => button.addEventListener("click", function(e) {
         
 if(button.innerText == ".") {
@@ -26,74 +25,67 @@ if(button.innerText == ".") {
 if(button.innerText == "%") {
 
 }
-
-
-
 if(currentNumber.innerText > 1000000000000000) {
         currentNumber.innerText = currentNumber.innerText
 }
         currentNumber.innerText = currentNumber.innerText + button.innerText
 }
-
 ));
 
-//let counter = 0;
-let c = 0
-let d = 1
-
 operationSymbols.forEach(button => button.addEventListener("click",function(e) {
+        
         counter++
         currentOperationSymbol = button.innerText
         currentBoardSymbol.innerText = currentOperationSymbol
-        if(currentNumber.innerText !== "") {
-        numbersArr.push(currentNumber.innerText)
-        }
-        previousNumber.innerText = currentNumber.innerHTML
-        currentNumber.innerText = currentNumber.innerText
+           if((currentNumber.innerText == "")&&(previousNumber.innerText == "")) {
+                currentNumber.innerText = 0 
+
+           }
+           if(currentNumber.innerText !== "") {
+                numbersArr.push(currentNumber.innerText)
+                }
+        previousNumber.innerText = numbersArr.at(-1) 
         symbolArr.push(currentOperationSymbol)
-     
-if(numbersArr.length > 1) {
-if(numbersArr.at(-1) != numbersArr.at(-2)) {
-        if(currentNumber.innerText !== "")
-               r = operations[symbolArr.at(-2)](parseFloat(numbersArr.at(-2)),parseFloat(numbersArr.at(-1))) 
-               previousNumber.innerText = r
-               currentNumber.innerText = r
-               numbersArr.push(r)    
-                      
-}
- 
+
 if((currentBoardSymbol.innerText == "x²")||(currentBoardSymbol.innerText == "√")) {
         r = operations[symbolArr.at(-1)](parseFloat(numbersArr.at(-1))) 
         previousNumber.innerText = r
-        currentNumber.innerText = r 
-        currentBoardSymbol.innerText = ""
+        currentNumber.innerText = ""
         numbersArr.push(r)   
 }
 if((currentNumber.innerText == "%")) {
-        console.log(operations[symbolArr.at(-1)])
-        console.log(numbersArr.at(-1))
         r = operations[symbolArr.at(-1)](parseFloat(numbersArr.at(-1)))
         previousNumber.innerText = r
-        currentNumber.innerText = r
         currentBoardSymbol.innerText = ""
         numbersArr.push(r)  
 }
-}
+if(numbersArr.length > 1) {
+                if(currentNumber.innerText !== "") {
+                       r = operations[symbolArr.at(-2)](parseFloat(numbersArr.at(-2)),parseFloat(numbersArr.at(-1))) 
+                       previousNumber.innerText = r
+                       numbersArr.push(r)                 
+        }
+        }
 currentNumber.innerText = ""
 }
-
 ))
+deleteLastNumberBtn.addEventListener("click",() => {
+        let deleteLastNumberArray = Array.from(currentNumber.innerText).shift()
+        currentNumber.innerText = deleteLastNumberArray
+})
 deleteAllButton.addEventListener("click", () => {
-        currentNumber.innerText = "0"
-        previousNumber.innerText = "0"
+        currentNumber.innerText = ""
+        previousNumber.innerText = ""
         currentBoardSymbol.innerText = ""
+        numbersArr = []
+        symbolArr = []
 })
 resultBtn.addEventListener("click",function(){
         if((currentOperationSymbol !== undefined) || (currentNumber.innerText !== "")) {
-        result = operations[currentOperationSymbol](parseFloat(previousNumber.innerText),parseFloat(currentNumber.innerText))
+        result = operations[currentBoardSymbol.innerText](parseFloat(numbersArr.at(-1)),parseFloat(currentNumber.innerText))
         currentNumber.innerText = result
         previousNumber.innerText = ""
-
+        currentBoardSymbol.innerText = ""
         }
   })
 const operations = {
@@ -104,5 +96,4 @@ const operations = {
         "÷" : (x,y) => x / y,
         "x²" : (x) => x * x,
         "√" : (x) => Math.sqrt(x),
-
 }
